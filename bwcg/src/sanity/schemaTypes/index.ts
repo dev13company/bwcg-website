e5bce5cd90fb8a7e8ea73069e71b566fdf4030c6
Avinash <avinash.thatikonda@t-systems.com>
2025-11-05 16:16:05 +0530

    commit

--- bwcg/src/sanity/schemaTypes/index.ts
+++ bwcg/src/sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity'
 import heroSection from './heroSection';
 import galleryImage from './galleryImage';
 import meetingSection from './meetingSection';
+import aboutUsSection from "./aboutUsSection";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [heroSection, galleryImage, meetingSection],
+  types: [heroSection, galleryImage, meetingSection, aboutUsSection],
 }
