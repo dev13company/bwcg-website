92d0e7b5a7fbc32685c2c7edd304f8a95199d440
Avinash Thatikonda <avinash.thatikonda@t-systems.com>
2025-12-06 15:28:21 +0530

    commit

--- bwcg/sanity.config.ts
+++ bwcg/sanity.config.ts
@@ -6,7 +6,9 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
+import { deskTool } from 'sanity/desk';
 import {structureTool} from 'sanity/structure'
+// import schemaTypes from './src/sanity/schemaTypes';
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
@@ -14,9 +16,10 @@ import {schema} from './src/sanity/schemaTypes'
 import {structure} from './src/sanity/structure'
 
 export default defineConfig({
-  basePath: '/studio',
+  title: 'BWC Gachibowli Studio',
   projectId,
   dataset,
+  basePath: '/studio',
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
@@ -24,5 +27,6 @@ export default defineConfig({
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
+    deskTool()
   ],
 })
--- bwcg/src/app/page.tsx
+++ bwcg/src/app/page.tsx
@@ -3,18 +3,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from 'react';
-import { createClient } from 'next-sanity';
+import {client} from '../sanity/lib/client'
 import imageUrlBuilder from '@sanity/image-url';
 import { FaFacebook, FaInstagram, FaYoutube, FaPhone, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 import galleryImage from "@/sanity/schemaTypes/galleryImage";
-
-
-const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? '',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? '',
-  apiVersion: '2023-07-11',
-  useCdn: true,
-});
 
 const builder = imageUrlBuilder(client);
 function urlFor(source: any) {
