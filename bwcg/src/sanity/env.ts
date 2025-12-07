1325feb5328165e96e5a752519c9c3bf276a16a9
Avinash Thatikonda <avinash.thatikonda@t-systems.com>
2025-12-07 19:53:29 +0530

    commit

--- bwcg/src/sanity/env.ts
+++ bwcg/src/sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-10-23'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET||'production',
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID||'e2yi0er0',
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
